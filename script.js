4f1a47caff2030fb6a349b4c076852f14641d058
宮本咲 <miyamotosaki@miyamotosakinoMacBook-Air.local>
2024-01-10 12:02:56 +0900

    show balance

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const symbol = require('/node_modules/symbol-sdk')
-
+const GENERATION_HASH = '49D6E1CE276A85B70EAFE52349AACCA389302E7A9754BCF1221E79494FC665A4'
+const EPOCH = 1667250467
+const XYM_ID = '72C0212E67A08BCE'
+const NODE_URL = 'https://sym-test-03.opening-line.jp:3001'
+const NET_TYPE = symbol.NetworkType.TEST_NET
 const address = symbol.Address.createFromRawAddress("TCRPYAX4AUWTCTKDOJS4LRSNN6EAIZLT552LTRY")
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
+
+const repositoryFactory = new symbol.RepositoryFactoryHttp(NODE_URL)
+const accountHttp = repositoryFactory.createAccountRepository()
+
+
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
+
+accountHttp.getAccountInfo(address)
+  .toPromise()
+  .then((accountInfo) => {
+    for (let m of accountInfo.mosaics) {
+      if (m.id.id.toHex() === XYM_ID) {
+        const dom_xym = document.getElementById('wallet-xym')
+        dom_xym.innerText = `XYM Balance : ${m.amount.compact() / Math.pow(10, 6)}`
+      }
+    }
+  })
