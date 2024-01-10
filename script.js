7bb228f3a258c35cabe13f34588570dde59f1d04
宮本咲 <miyamotosaki@miyamotosakinoMacBook-Air.local>
2024-01-10 11:52:09 +0900

    show Address

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TCRPYAX4AUWTCTKDOJS4LRSNN6EAIZLT552LTRY")
 console.log("Hello Symbol")
+console.log(`Your Address : ${address.plain()}`)
